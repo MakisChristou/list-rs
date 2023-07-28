f19fe3059bd56dc857091683e0ff536005cf7912
MakisChristou <michalis.christou@protonmail.com>
2023-07-28 22:26:18 +0300

    Move Database Handler to its own module

---
+++ src/db_handler.rs
@@ -0,0 +1,68 @@
+use rusqlite::{types::FromSql, Connection, Result};
+
+use crate::task::Task;
+
+pub struct DatabaseHandler {
+    pub conn: Connection,
+}
+
+impl DatabaseHandler {
+    pub fn new(database_path: &str) -> Self {
+        let conn = Connection::open(database_path).unwrap();
+
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS Tasks (
+                id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                title       TEXT NOT NULL,
+                text        TEXT NOT NULL,
+                status      TEXT NOT NULL,
+                tag         TEXT,
+                due_date    TEXT
+            )",
+            (), // empty list of parameters.
+        )
+        .unwrap();
+
+        DatabaseHandler { conn }
+    }
+
+    pub fn create_task(&self, task: Task) {
+        self.conn.execute(
+            "INSERT INTO Tasks (title, text, status, tag, due_date) VALUES (?1, ?2, ?3, ?4, ?5)",
+            (
+                &task.title,
+                &task.text,
+                &task.status.to_string(),
+                &task.tag,
+                &task.due_date,
+            ),
+        ).unwrap();
+    }
+
+    pub fn read_tasks(&self) -> Vec<Task> {
+        let mut stmt = self
+            .conn
+            .prepare("SELECT id, title, text, status, tag, due_date FROM Tasks")
+            .unwrap();
+        let task_iter = stmt
+            .query_map([], |row| {
+                Ok(Task {
+                    id: row.get(0)?,
+                    title: row.get(1)?,
+                    text: row.get(2)?,
+                    status: row.get(3)?,
+                    tag: row.get(4)?,
+                    due_date: row.get(5)?,
+                })
+            })
+            .unwrap();
+
+        let mut tasks = Vec::new();
+
+        for task in task_iter {
+            tasks.push(task.unwrap());
+        }
+
+        tasks
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use rusqlite::{types::FromSql, Connection, Result};
+use rusqlite::{types::FromSql, Result};
 
+mod db_handler;
 mod task;
+
+use crate::db_handler::DatabaseHandler;
 use crate::task::Task;
 use crate::task::TaskStatus;
 
 fn main() -> Result<()> {
-    let conn = Connection::open("tasks.db")?;
+    let db_handler = DatabaseHandler::new("tasks.db");
 
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS Tasks (
-            id          INTEGER PRIMARY KEY AUTOINCREMENT,
-            title       TEXT NOT NULL,
-            text        TEXT NOT NULL,
-            status      TEXT NOT NULL,
-            tag         TEXT,
-            due_date    TEXT
-        )",
-        (), // empty list of parameters.
-    )?;
     let default_task = Task {
         id: 0,
         title: "Task title".to_string(),
@@ -26,31 +18,12 @@ fn main() -> Result<()> {
         tag: None,
         due_date: None,
     };
-    conn.execute(
-        "INSERT INTO Tasks (title, text, status, tag, due_date) VALUES (?1, ?2, ?3, ?4, ?5)",
-        (
-            &default_task.title,
-            &default_task.text,
-            &default_task.status.to_string(),
-            &default_task.tag,
-            &default_task.due_date,
-        ),
-    )?;
 
-    let mut stmt = conn.prepare("SELECT id, title, text, status, tag, due_date FROM Tasks")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Task {
-            id: row.get(0)?,
-            title: row.get(1)?,
-            text: row.get(2)?,
-            status: row.get(3)?,
-            tag: row.get(4)?,
-            due_date: row.get(5)?,
-        })
-    })?;
+    db_handler.create_task(default_task);
 
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
+    let tasks = db_handler.read_tasks();
+
+    println!("All Tasks: {:?}", tasks);
+
     Ok(())
 }
